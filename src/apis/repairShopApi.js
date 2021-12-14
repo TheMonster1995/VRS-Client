deae689bc19e6c768a5f1a124a1592f2f510cbb0
Fo'ad (Mnstr) Salmanian <salmanian.foad2@gmail.com>
2021-12-14 15:58:26 +0330

    Update repairShopApi.js

--- src/apis/repairShopApi.js
+++ src/apis/repairShopApi.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://51.75.182.106:4050'
+  baseURL: 'http://SERVER_API:4050'
 })
-
-// baseURL: 'http://51.75.182.106:4050'
-// baseURL: 'http://localhost:4050'
